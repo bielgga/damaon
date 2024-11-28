f61e6cdd87f3a9919558043c422924e67167f4dd
Gabriel <biellgga@gmail.com>
2024-11-27 22:12:18 -0300

    Corrige problemas de dependências

--- vite.config.ts
+++ vite.config.ts
@@ -5,9 +5,6 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   build: {
-    rollupOptions: {
-      external: ['@rollup/rollup-linux-x64-gnu'],
-    },
     commonjsOptions: {
       transformMixedEsModules: true,
       include: [
