c5f0feca508e0e77bdfde1913616bf181b3427dd
Gabriel <biellgga@gmail.com>
2024-11-27 22:08:19 -0300

    Fix build configuration

--- vite.config.ts
+++ vite.config.ts
@@ -4,12 +4,18 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  optimizeDeps: {
-    exclude: ['@rollup/rollup-linux-x64-gnu']
-  },
   build: {
+    rollupOptions: {
+      external: ['@rollup/rollup-linux-x64-gnu'],
+    },
     commonjsOptions: {
-      include: []
+      transformMixedEsModules: true,
+      include: [
+        /node_modules/
+      ]
     }
+  },
+  optimizeDeps: {
+    include: ['react', 'react-dom', '@dnd-kit/core', '@dnd-kit/utilities']
   }
 });
